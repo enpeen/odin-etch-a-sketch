bff9872b58d8aacc59da83e3e47e203a4455dd4f
enpeen <enpeen0@gmail.com>
2023-06-11 21:13:13 +0200

    Solution in progress

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 const container = document.getElementById("container");
+
+let gridSize = 256;
+
+for (let i = 0; i < gridSize; i++) {
+    let div = document.createElement("div");
+    div.style.border = "2px solid black";
+    div.style.width = "58px";
+    div.style.height = "58px";
+    div.textContent = "etch a sketch";
+    container.appendChild(div);
+}
