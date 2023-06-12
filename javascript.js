3e3e9df96352140c4f26f4c483e3c52a99fc575d
enpeen <enpeen0@gmail.com>
2023-06-12 18:19:55 +0200

    Solution in progress

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
 const container = document.getElementById("container");
+container.style.width = "640px";
+container.style.height = "640px";
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
 
-let gridSize = 256;
 
-for (let i = 0; i < gridSize; i++) {
-    let div = document.createElement("div");
-    div.style.border = "2px solid black";
-    div.style.width = "58px";
-    div.style.height = "58px";
-    div.textContent = "etch a sketch";
-    container.appendChild(div);
+let gridSize = prompt("How many per side");
+let calc1 = 640 / gridSize;
+let calc2 = 640 / gridSize;
+
+for (let i = 0; i < (gridSize * gridSize); i++) {
+    const innerDiv = document.createElement("div");
+    innerDiv.className = "innerDiv";
+    innerDiv.textContent = "etch";
+    innerDiv.style.color = "blue";
+    innerDiv.style.flexGrow = "1";
+    innerDiv.style.width = calc1 + "px";
+    innerDiv.style.height = calc2 + "px";
+
+
+    container.appendChild(innerDiv);
 }
